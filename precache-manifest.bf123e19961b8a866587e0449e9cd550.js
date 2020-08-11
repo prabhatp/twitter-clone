8f961feb11fea6c1fcd385026da3bf69a3737f29
prabhatp <prabhakarprabhat523@gmail.com>
2020-08-11 07:30:50 +0530

    Updates

---
+++ precache-manifest.bf123e19961b8a866587e0449e9cd550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549c5cd06c5f50d101b8ab19285be451",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "5ee28cecf2d83938a3f8",
+    "url": "/twitter-clone/static/css/main.4973f57a.chunk.css"
+  },
+  {
+    "revision": "48466e9e9ae509a8ba03",
+    "url": "/twitter-clone/static/js/2.1c430df2.chunk.js"
+  },
+  {
+    "revision": "698c7d1ab7fc994ffbdcf6ff99e91ff0",
+    "url": "/twitter-clone/static/js/2.1c430df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee28cecf2d83938a3f8",
+    "url": "/twitter-clone/static/js/main.74da55d2.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
